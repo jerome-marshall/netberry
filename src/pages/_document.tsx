86a5237b334c4533ebcb87ea031fb26f65f1144f
Jerome Marshall <jeromemarshall0@gmail.com>
2023-03-06 00:31:17 +0530

    ✨ NEW: favicon

---
+++ src/pages/_document.tsx
@@ -0,0 +1,41 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import { ReactElement } from "react";
+
+// NEXT.JS CUSTOM DOCUMENT
+// https://nextjs.org/docs/advanced-features/custom-document
+
+export default class MyDocument extends Document {
+  render(): ReactElement {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+          <meta name="msapplication-TileColor" content="#2b5797" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
