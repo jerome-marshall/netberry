78db2b2a456b6a623364ce47425fcbcc7cf54f27
Jerome Marshall <jeromemarshall0@gmail.com>
2023-03-11 22:44:30 +0530

    ⚡ IMPROVE: pagination module

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -15,7 +15,6 @@ const Pagination = ({
 }: PaginationProps) => {
   const pageNumbers: number[] = [];
 
-  // Generate page numbers
   for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
     pageNumbers.push(i);
   }
@@ -23,49 +22,40 @@ const Pagination = ({
   const totalPageNumbers = pageNumbers?.length;
 
   const handlePageChange = (value: number | string) => {
-    // handle change for number based page change
-    if (typeof value === "number") setCurrentPage(value);
-    else
-      switch (value) {
-        case "back":
-          setCurrentPage((page) => --page);
-          break;
-        case "next":
-          setCurrentPage((page) => ++page);
-          break;
-        default:
-          break;
-      }
+    switch (value) {
+      case "back":
+        setCurrentPage((page) => --page);
+        break;
+      case "next":
+        setCurrentPage((page) => ++page);
+        break;
+      default:
+        break;
+    }
   };
 
   if (totalPageNumbers < 2) return null;
 
   return (
-    <div className="pagination-container mx-auto mt-6 flex items-center justify-center gap-6">
+    <div className="mx-auto mt-6 flex items-center justify-center gap-6">
       <button
         onClick={() => handlePageChange("back")}
-        className={`button button-secondary pagination-button button-back ${
-          currentPage <= 1 ? "button-disabled" : ""
-        }`}
+        className={`button`}
         disabled={currentPage <= 1}
       >
-        <i className="icon icon-arrow-left"></i>
         <span>Back</span>
       </button>
 
-      <div className="pagination-numbers-wrap">
+      <div className="">
         Page {currentPage} of {totalPageNumbers}
       </div>
 
       <button
         onClick={() => handlePageChange("next")}
-        className={`button button-secondary pagination-button button-next ${
-          currentPage >= totalPageNumbers ? "button-disabled" : ""
-        }`}
+        className={`button`}
         disabled={currentPage >= totalPageNumbers}
       >
         <span>Next</span>
-        <i className="icon icon-arrow-right"></i>
       </button>
     </div>
   );
