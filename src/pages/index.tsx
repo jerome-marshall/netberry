3f886e239278062dc64dff2197218b10422f18c5
Jerome Marshall <jeromemarshall0@gmail.com>
2023-03-20 16:03:41 +0530

    ⏫ UPDATE: gssp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,6 @@ const Home: NextPage = () => {
   );
 };
 
-export const getServerSideProps = getServerSidePropsHelper;
+// export const getServerSideProps = getServerSidePropsHelper;
 
 export default Home;
