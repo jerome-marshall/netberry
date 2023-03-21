cace0ad99be19bfee6f734aa05346890e821df16
Jerome Marshall <jeromemarshall0@gmail.com>
2023-03-21 18:06:51 +0530

    ✨ NEW: authored by

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 import type { FC } from "react";
 import starberryLogo from "../assets/starberry-landscape-logo-white.png";
+import { Tooltip } from "react-tooltip";
+import "react-tooltip/dist/react-tooltip.css";
+import { tootTipStyle } from "../common/utils";
 
 // interface HeaderProps {
 //   children: React.ReactNode;
@@ -10,19 +13,38 @@ import starberryLogo from "../assets/starberry-landscape-logo-white.png";
 const Footer: FC = () => {
   const year = new Date().getFullYear();
   return (
-    <div className="footer flex items-center justify-between py-10">
+    <div className="footer flex items-end justify-between py-10">
       <div className="text-sm text-text-muted">Copyrights &copy; {year}</div>
-      <div className="flex items-center text-sm text-text-muted">
-        <span>NetBerry, a product of</span>{" "}
-        <Link href={"https://starberry.tv/"} target="_blank">
-          <Image
-            src={starberryLogo}
-            width={96}
-            height={40}
-            alt="starberry-logo"
-          />
-        </Link>
+      <div className="flex flex-col items-end">
+        <div className="flex items-center text-sm text-text-muted">
+          <span>NetBerry, a product of</span>{" "}
+          <Link
+            href={"https://starberry.tv/"}
+            target="_blank"
+            className="relative top-[1px]"
+          >
+            <Image
+              src={starberryLogo}
+              width={96}
+              height={40}
+              alt="starberry-logo"
+            />
+          </Link>
+        </div>
+        <div className="relative right-1 flex items-center text-sm text-text-muted">
+          <span>Authored by</span>
+          <Link
+            href={"https://jerome-marshall.github.io/"}
+            target="_blank"
+            className=" ml-0.5 hover:text-white/90 hover:underline"
+            data-tooltip-id="footer-tooltip"
+            data-tooltip-content="Jerome Marshall"
+          >
+            JM
+          </Link>
+        </div>
       </div>
+      <Tooltip id="footer-tooltip" style={tootTipStyle} />
     </div>
   );
 };
